23a1fa43256c78b5eeda493001d0e0f2550ff775
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2025-04-09 22:42:35 +0530

    Update 12 find smallest prime factor for a query of numbers.cpp

--- 12 find smallest prime factor for a query of numbers.cpp
+++ 12 find smallest prime factor for a query of numbers.cpp
@@ -1,7 +1,8 @@
 //sieve or erat lagao
 //for multiple of a prime number -> store that prime no itself
 
-//check file '10 sieve_of_erat_all primes smaller than or equal to n.cpp' -> isme -> prime[j] = false -> ki jagah -> prime[j] = i karo
+//check file '10 sieve_of_erat_all primes smaller than or equal to n.cpp'
+//usme -> prime[j] = false -> ki jagah -> prime[j] = i karo
 
 //eg -> 1,2,3,4,5,6,7,8,9,10,11,12,13....
      -> -,2,3,2,5,2,7,2,3,2,11,2,13....
