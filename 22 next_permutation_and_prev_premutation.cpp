c5cdf997e433529f84f77e373222cbd4bd3e3d5b
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2025-04-15 22:27:32 +0530

    Update 22 next_permutation_and_prev_premutation.cpp

--- 22 next_permutation_and_prev_premutation.cpp
+++ 22 next_permutation_and_prev_premutation.cpp
@@ -5,23 +5,17 @@
 //used to rearrange the elements in the range [first, last) into the next lexicographically greater permutation
 //NOTE => see above => last index is NOT included
 
-int arr[] = { 1, 2, 3 };
-
-do {
-    cout << arr[0] << " " << arr[1] << " " << arr[2] << "\n";
-} while (next_permutation(arr, arr + 3));
-
+vector<int>v = {4,3,2,5,6,1};
+next_permutation(v.begin(), v.end());
+for(auto x:v)
+    cout<<x<<" ";  //op: 4 3 2 6 1 5 
 
 
 // prev_permutation:
 // used to rearranges the elements in the range [first, last) into the previous lexicographically-ordered permutation
 //NOTE => see above => last index is NOT included
 
-int arr[] = { 1, 2, 3 };
- 
-sort(arr, arr + 3);
-reverse(arr, arr + 3);  //sort and reverse since we want prev perm
-
-do {
-    cout << arr[0] << " " << arr[1] << " " << arr[2] << "\n";
-} while (prev_permutation(arr, arr + 3));
+vector<int>v = {4,3,2,5,6,1};
+prev_permutation(v.begin(), v.end());
+for(auto x:v)
+    cout<<x<<" ";  //op: 4 3 2 5 1 6 
