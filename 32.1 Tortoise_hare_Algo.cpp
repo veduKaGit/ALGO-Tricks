539e1b8cb126340fd737b4b2ec1a55c7e954d456
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2025-04-20 01:11:47 +0530

    Create 32.1 Tortoise_hare_Algo.cpp

---
+++ 32.1 Tortoise_hare_Algo.cpp
@@ -0,0 +1,57 @@
+// Tortoise Hare Algo
+// The same slow/fast ptr algo used in Detect_1st_node_of_cycle_in_linked_list
+
+// Another variation:
+// https://leetcode.com/problems/find-the-duplicate-number/description/
+
+why => 32 all_occur_k_times_but_one_occurs_p_times.cpp => does NOT work here???
+because here p%k=0
+
+// since we can't use linear extra space => use Tortoise Hare
+
+// why will this algo work? 
+// where is a cycle formed? 
+// why our answer is the starting node of the cycle?
+
+// say our array is => [2, 4, 1, 1, 3]
+// if we make a linked list of this array like:
+// node[0] -> node[node[0]] -> node[node[node[0]]] .... so on ....
+// it will look like: 2 -> 1 -> 4 -> 3 -> 1 -> 4 -> 3 -> 1 .... so on ....
+// we find a cycle => 1 -> 4 -> 3 -> 1
+// this cycle starts at 1 => thats our answer
+
+
+// NOTE: the below soln works even when:
+// we have array of size n+1
+// all numbers are in the range [1,n]
+// multiple numbers can repeat multiple times
+// we can return ANY number that is repeating
+// example => [1, 4, 4, 3, 6, 4, 3, 3]
+// our soln will work here as well
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& A) {
+        if (A.size() <= 1) 
+            return -1;
+        
+        int slow = A[0]; // IMP => initialize both as A[0] => since linked list starts from there
+        int fast = A[0];
+
+        slow = A[slow];  // IMP => do this once before entering while loop => so that slow!=fast in the very 1st iteration
+        fast = A[A[fast]];
+
+        while(slow!=fast){
+            slow = A[slow];
+            fast = A[A[fast]];
+        };
+
+        fast = A[0];
+        while (slow != fast) {
+            slow = A[slow];
+            fast = A[fast];
+        }
+
+        return fast;
+    }
+};
