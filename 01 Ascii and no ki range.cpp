fe8b32845604e135c8d0226a222a5d0dc8f2d1b7
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2025-05-18 01:23:34 +0530

    Update 01 Ascii and no ki range.cpp

--- 01 Ascii and no ki range.cpp
+++ 01 Ascii and no ki range.cpp
@@ -1,6 +1,15 @@
 1. ASCII....0->48
             a->97
             A->65
+
+2. If we have lowercase letters ONLY
+   make vector v of size 26
+   do v[str[i]-'a'] to get index
    
-2. int range-> { -2147483648 to 2147483647 } -> ie. { 2.1 * 10^9 tak ja sakta hai}
+3. If we have lowercase/uppercase/numbers/special characters 
+   make array of size 128 (all possible characters)
+   vector<int>v(128, -1);
+   v[str[i]] = ...something...;  // no need to do v[str[i]-'a'] => gives error
+   
+4. int range-> { -2147483648 to 2147483647 } -> ie. { 2.1 * 10^9 tak ja sakta hai}
    long long int range -> { -(2^63) to (2^63)-1 } -> ie. {  9 * 10^18 tak ja sakta hai}
